74098740bd21fbc6f325ee9ff36ecac296afd577
aadityap0ap <adityabharti450ap@gmail.com>
2026-04-19 11:37:02 +0530

    Skeleton Routes Created

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,32 @@
 import express from "express";
-import mongoose  from "mongoose";
-import jwt from "jsonwebtoken";
+
 const app = express();
+app.use(express.json());
+
+app.post("/signup",(req,res) => {
+
+})
+
+app.post("/signin",(req,res) => {
+
+})
+
+app.post("/content",(req,res) => {
+
+})
+
+app.get("/content",(req,res) => {
+
+})
+
+app.delete("/content",(req,res) => {
+
+})
+
+app.post("/brain/share",(req,res) => {
+
+})
+
+app.get("/brain/sharedLink",(req,res) => {
+    
+})
